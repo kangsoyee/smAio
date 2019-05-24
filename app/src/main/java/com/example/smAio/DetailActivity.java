e6fb0837d6d7c6009fdd80a8dc3a191f8954c683
kangsoyee <kangse2942@gmail.com>
2019-05-24 21:53:02 +0900

    DetailActivity 리뷰 탭에 리뷰리스트 띄우기

--- app/src/main/java/com/example/smAio/DetailActivity.java
+++ app/src/main/java/com/example/smAio/DetailActivity.java
@@ -31,15 +31,51 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DetailActivity extends AppCompatActivity {
     private ArrayList<HashMap<String,String>> Data1 = new ArrayList<HashMap<String, String>>();
     private HashMap<String,String> InputData1 = new HashMap<>();
-
     ImageView iv;
     boolean change=false;
+
+    int place_idx;
+    TextView txtCategory, txtPlaceName, txtStartTime, txtEndTime, txtAddress, txtTel, txtReview, txtMenu, txtPrice;
+    PlaceDTO placeInfo;
+    ArrayList<ReviewDTO> review_list=new ArrayList<>();
+    ListView list;
+
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(msg.what == 1) { //상세
+                txtAddress.setText(placeInfo.getAddress());
+                txtCategory.setText(placeInfo.getCategory());
+                txtEndTime.setText(placeInfo.getEnd_time());
+                txtStartTime.setText(placeInfo.getStart_time());
+                txtTel.setText(placeInfo.getTel());
+                txtPlaceName.setText(placeInfo.getPlace_name());
+                txtMenu.setText(placeInfo.getMenu());
+                txtPrice.setText(placeInfo.getPrice());
+            }else if(msg.what == 2){ //수정,삭제
+                finish();
+            }else if(msg.what == 3){ //리뷰 목록
+                ReviewAdapter adapter = new ReviewAdapter(
+                        DetailActivity.this,
+                        R.layout.review_row,
+                        review_list);       // , 로 두가지를 집어넣어도 되는건가?
+                list.setAdapter(adapter);
+            }else if(msg.what==4){
+                txtReview.setText("");
+                review_list();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,25 +101,6 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        ListView listView2 = (ListView) findViewById(R.id.ListView2);
-        //데이터 초기화
-        InputData1.put("name","김수연");
-        Data1.add(InputData1);
-
-        //simpleAdapter 생성
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,Data1,android.R.layout.simple_list_item_2,
-                new String[]{"name","review"},
-                new int[]{android.R.id.text1,android.R.id.text2});
-
-        //InputData1.put("review","너무 맛있어요~");
-        //listView2.setAdapter(simpleAdapter);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        StoreListFragment fragment5 = new StoreListFragment();
-        transaction.replace(R.id.storelist_container, fragment5);
-        transaction.commit();
-//        list=(ListView)findViewById(R.id.list);
-//        review_list();
-
         //별점
         final TextView tv = (TextView) findViewById(R.id.textView4);
         RatingBar rb = (RatingBar) findViewById(R.id.ratingBar);
@@ -105,11 +122,9 @@ public class DetailActivity extends AppCompatActivity {
         TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("tab1");
         tabSpec1.setIndicator("정보");
         tabSpec1.setContent(R.id.tabSpec1);
-
         TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("tab2");
         tabSpec2.setIndicator("리뷰");
         tabSpec2.setContent(R.id.tabSpec2);
-
         TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("tab3");
         tabSpec3.setIndicator("지도");
         tabSpec3.setContent(R.id.tabSpec3);
@@ -121,8 +136,174 @@ public class DetailActivity extends AppCompatActivity {
 
         //초기 Tab 설정
         tabHost.setCurrentTab(0);
+
+
+        ListView listView2 = (ListView) findViewById(R.id.detail_info_list);
+
+        //simpleAdapter 생성
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,Data1,android.R.layout.simple_list_item_2,
+                new String[]{"name","review"},
+                new int[]{android.R.id.text1,android.R.id.text2});
+
+        //데이터 초기화
+        InputData1.put("name","김수연");
+        InputData1.put("review","너무 맛있어요~");
+        Data1.add(InputData1);
+        listView2.setAdapter(simpleAdapter);
+
+
+        txtStartTime=(TextView) findViewById(R.id.start_time);
+        txtEndTime=(TextView) findViewById(R.id.end_time);
+        txtPlaceName=(TextView) findViewById(R.id.place_name);
+        list=(ListView)findViewById(R.id.detail_review_list);
+
+        Intent intent=getIntent();
+        place_idx=intent.getIntExtra("idx",0);
+        review_list();
+        detail();
+
     }
-/*
+
+
+    void review_list(){
+        //네트워크 관련 작업은 백그라운드 스레드에서 처리
+        final StringBuilder sb=new StringBuilder();
+
+
+        Thread th = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    review_list = new ArrayList<ReviewDTO>();
+                    String page = Common.SERVER_URL+"/review_list.php?place_idx="+place_idx;
+                    Log.e("DetailActivity","여기까지는 이동함");
+
+                    URL url = new URL(page);
+                    // 커넥션 객체 생성
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    // 연결되었으면.
+                    if (conn != null) {
+                        //타임아웃 시간 설정
+                        conn.setConnectTimeout(10000);
+                        //캐쉬 사용 여부
+                        conn.setUseCaches(false);
+                        //url에 접속 성공하면
+                        if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                            //스트림 생성
+                            BufferedReader br=
+                                    new BufferedReader(
+                                            new InputStreamReader(
+                                                    conn.getInputStream(),"utf-8"));
+                            while(true){
+                                String line=br.readLine(); //한 라인을 읽음
+                                if(line == null) break;//더이상 내용이 없으면 종료
+                                sb.append(line+"\n");
+                            }
+                            br.close(); //버퍼 닫기
+                        }
+                        conn.disconnect();
+                    }
+// 스트링을 json 객체로 변환
+                    JSONObject jsonObj = new JSONObject(sb.toString());
+                    Log.i("test","review_list:"+sb);
+// json.get("변수명")
+                    JSONArray jArray = (JSONArray) jsonObj.get("sendData");     //sendData 어떻게 쓰이는지
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject row = jArray.getJSONObject(i);
+                        ReviewDTO dto = new ReviewDTO();
+                        dto.setReview_content(row.getString("review_content"));
+                        review_list.add(dto);
+                    }
+                    //핸들러에게 화면 갱신 요청
+                    handler.sendEmptyMessage(3);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        th.start();
+    }
+    class ReviewAdapter extends ArrayAdapter<ReviewDTO> {
+        public ReviewAdapter(Context context, int textViewResourceId,
+                             ArrayList<ReviewDTO> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater li = (LayoutInflater)
+                        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = li.inflate(R.layout.review_row, null);
+            }
+            final ReviewDTO dto = review_list.get(position);
+            Log.i("test","review dto:"+dto);
+            Log.i("test","review content:"+dto.getReview_content());
+            if (dto != null) {
+                TextView review_content =(TextView) v.findViewById(R.id.review_content);
+                review_content.setText(dto.getReview_content());
+            }
+            return v;
+        }
+    }
+    void detail(){
+        //네트워크 관련 작업은 백그라운드 스레드에서 처리
+        final StringBuilder sb=new StringBuilder();
+        Thread th = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    String page = Common.SERVER_URL+"/place_detail.php?place_idx="+place_idx;
+
+                    URL url = new URL(page);
+                    // 커넥션 객체 생성
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    // 연결되었으면.
+                    if (conn != null) {
+                        //타임아웃 시간 설정
+                        conn.setConnectTimeout(10000);
+                        //캐쉬 사용 여부
+                        conn.setUseCaches(false);
+                        //url에 접속 성공하면
+                        if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+                            //스트림 생성
+                            BufferedReader br=
+                                    new BufferedReader(
+                                            new InputStreamReader(
+                                                    conn.getInputStream(), StandardCharsets.UTF_8));
+                            while(true){
+                                String line=br.readLine(); //한 라인을 읽음
+                                if(line == null) break;//더이상 내용이 없으면 종료
+                                sb.append(line+"\n");
+                            }
+                            br.close(); //버퍼 닫기
+                        }
+                        conn.disconnect();
+                    }
+// 스트링을 json 객체로 변환
+                    JSONObject jsonObj = new JSONObject(sb.toString());
+
+// json.get("변수명")
+                    JSONObject row = (JSONObject)jsonObj.get("sendData");
+                    placeInfo = new PlaceDTO();
+                    placeInfo.setPlace_idx(row.getInt("place_idx"));
+                    placeInfo.setEnd_time(row.getString("end_time"));
+                    placeInfo.setStart_time(row.getString("start_time"));
+                    placeInfo.setPlace_name(row.getString("place_name"));
+                    //핸들러에게 화면 갱신 요청
+                    handler.sendEmptyMessage(1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        th.start();
+    }
+
+    /*
+
+    //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+
     int place_idx;
 
     EditText txtCategory, txtPlaceName, txtStartTime, txtEndTime, txtAddress, txtTel, txtReview, txtMenu, txtPrice;
@@ -159,7 +340,7 @@ public class DetailActivity extends AppCompatActivity {
             }
         }
     };
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -241,7 +422,7 @@ public class DetailActivity extends AppCompatActivity {
                             BufferedReader br=
                                     new BufferedReader(
                                             new InputStreamReader(
-                                                    conn.getInputStream(),"UTF-8"));
+                                                    conn.getInputStream(), StandardCharsets.UTF_8));
                             while(true){
                                 String line=br.readLine(); //한 라인을 읽음
                                 if(line == null) break;//더이상 내용이 없으면 종료
@@ -476,4 +657,3 @@ public class DetailActivity extends AppCompatActivity {
         }
     }*/
 }
-
