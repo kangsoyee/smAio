56f01ed1fab533527d7a99b33d046a64572b3c76
kangsoyee <kangse2942@gmail.com>
2019-05-28 21:13:02 +0900

    FirstActivity에 권한 허용 팝업 추가

--- app/src/main/java/com/example/smAio/FirstActivity.java
+++ app/src/main/java/com/example/smAio/FirstActivity.java
@@ -1,9 +1,14 @@
 package com.example.smAio;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -96,6 +101,17 @@ public class FirstActivity extends AppCompatActivity {
         if(logout=="1"){
             sessionManager.logout();
         }
+
+        //카메라, 위치 권한 허용 팝업
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {Manifest.permission.CAMERA,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        if(!hasPermissions(this, PERMISSIONS)){
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
     }
     @Override
     public void onBackPressed() {
@@ -116,7 +132,6 @@ public class FirstActivity extends AppCompatActivity {
             }
         }
     }
-
 
     //프래그먼트 화면 변경 함수
     private void callFragment(int fragment_no){
@@ -234,5 +249,17 @@ public class FirstActivity extends AppCompatActivity {
         i.putExtra("boolcheck",false);
         startActivity(i);
         finish();
+    }
+
+    //카메라 권한 허용 함수
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
