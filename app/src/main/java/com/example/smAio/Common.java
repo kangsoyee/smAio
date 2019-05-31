e0cb47bb7722a63b89c6f5da5f5298b379505cdb
eileenyoo2 <eileenyoo2@gmail.com>
2019-05-31 21:33:31 +0900

    호스팅 서버 변경

--- app/src/main/java/com/example/smAio/Common.java
+++ app/src/main/java/com/example/smAio/Common.java
@@ -2,7 +2,7 @@ package com.example.smAio;
 
 public class Common {
     public static final String SERVER_URL
-            = "http://eileenyoo.cafe24.com";
+            = "http://eileenyoo1.cafe24.com";
     public static String userid;
     public static String name;
 }
