2a5f00fa99f9ccdbfa06b44ea1e5fa2c21657c92
KIMSY <rlatndus0904@naver.com>
2019-06-09 21:00:55 +0900

    //DetailActivity tabhost부분 주석 추가 //MyReviewActivity //MyReviewDTO

--- app/src/main/java/com/example/smAio/MyReviewDTO.java
+++ app/src/main/java/com/example/smAio/MyReviewDTO.java
@@ -1,5 +1,9 @@
 package com.example.smAio;
 
+//DB에 만들어진 필드랑 1:1 대응하는 변수 + getter & setter 를 가진 클래스를 DTO 라고 합니다.
+//데이터베이스의 review 테이블과 user 테이블을 사용하였으며 상점이름,사용자 id,해당 id로 남긴 리뷰를 가지고있다.
+// 사용 방법은 set을 통해 값을 설정하고, get을 통해 값을 가져옵니다.
+
 public class MyReviewDTO {
     private String place_name;
     private String username;
